8d99e8c32e2d7a54b22dc0493510be2f47e459cf
root <root@DESKTOP-6E3VB08.localdomain>
2022-08-15 11:15:15 -0300

    trying fix camera

--- src/controller/CameraController.js
+++ src/controller/CameraController.js
@@ -5,15 +5,19 @@ class CameraController{
 
         this._videoEl = videoEl;
 
-        navigator.mediaDevices.getUserMedia({
-            video: true
-        }).then(stream=>{
+        navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+
+            this._stream = stream;
 
             this._videoEl.src = URL.createObjectURL(stream);
+
             this._videoEl.play();
-        })
 
+        }).catch(err => {
 
+            console.error(err);
+
+        });
 
     }
 
