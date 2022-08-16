622c67c1c1a7e66de8da73cf9fbeb05514e0a0a0
root <root@DESKTOP-6E3VB08.localdomain>
2022-08-16 08:54:13 -0300

    add: icons on filetypes

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -1,5 +1,7 @@
-import {Format} from './../util/format.js'
-import {CameraController} from './CameraController.js'
+// import { timeStamp } from 'console';
+import { Format } from './../util/format.js'
+import { CameraController } from './CameraController.js'
+import { DocumentPreviewController } from './DocumentPreviewController.js'
 
 
 export class WhatsAppController {
@@ -124,26 +126,26 @@ export class WhatsAppController {
         })
 
         this.el.btnTakePicture.on('click', e => {
-
             let dataUrl = this._camera.takePicture();
+
             this.el.pictureCamera.src = dataUrl;
             this.el.pictureCamera.show();
             this.el.videoCamera.hide();
             this.el.btnReshootPanelCamera.show();
             this.el.containerTakePicture.hide();
             this.el.containerSendPicture.show();
+        });
 
-        })
-        this.btnReshootPanelCamera.on('click', e=>{
+        this.el.btnReshootPanelCamera.on('click', e => {
+
 
             this.el.pictureCamera.hide();
             this.el.videoCamera.show();
             this.el.btnReshootPanelCamera.hide();
             this.el.containerTakePicture.show();
             this.el.containerSendPicture.hide();
-
-        })
-        this.el.btnSendPicture.on('click', e=>{
+        });
+        this.el.btnSendPicture.on('click', e => {
 
             console.log(this.el.pictureCamera.src);
         })
@@ -162,8 +164,60 @@ export class WhatsAppController {
             this.el.panelDocumentPreview.css({
                 'height': '100%'
             })
+            this.el.inputDocument.click();
 
 
+
+        })
+
+        this.el.inputDocument.on('change', e => {
+
+            if (this.el.inputDocument.files.length) {
+
+                let file = this.el.inputDocument.files[0];
+
+                this._documentPreviewController = new DocumentPreviewController(file)
+
+                this._documentPreviewController.getPreviewData().then(result => {
+
+                    this.el.imgPanelDocumentPreview.src = result.src;
+                    this.el.infoPanelDocumentPreview.innerHTML = result.info;
+                    this.el.imagePanelDocumentPreview.show();
+                    this.el.filePanelDocumentPreview.hide();
+
+                }).catch(err => {
+
+                    switch (file.type) {
+
+                        case 'application/vnd.ms-excel':
+                        case 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+
+                            this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-xls'
+                            break;
+
+                        case 'application/vnd.mds-powerpoint':
+                        case 'application/vnd.openxmlformats-officedocument.presentationml.presentation':
+                            this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-ppt'
+                        break;
+
+                        case 'application/msword':
+                        case 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
+                            this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-doc'
+                        break;
+
+                        default:
+                            this.el.iconPanelDocumentPreview.className = 'jcxhw icon-doc-generic'
+                        break;
+
+                        
+
+                    }
+                    this.el.filenamePanelDocumentPreview.innerHTML = file.name;
+                    this.el.imagePanelDocumentPreview.hide();
+                    this.el.filePanelDocumentPreview.show();
+
+                })
+            }
 
         })
 
